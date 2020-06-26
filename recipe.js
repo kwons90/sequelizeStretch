4a365e12412fc34e0a3ebe8973f7436fb07f2e9f
Kwon <sanghyukkwon90@gmail.com>
2020-06-26 21:07:02 +0900

    why does it not work

--- recipe.js
+++ recipe.js
@@ -1,18 +1,25 @@
 const db = require('./dbConfig');
-const Sequelize = require('sequelize');
+const {Sequelize, UUID, UUIDV4} = require('sequelize');
 
-const Recipe = db.define('Lasagne', {
+const Recipe = db.define('Recipe', {
+    id: {
+        type: UUID,
+        defaultValue: UUIDV4,
+        primaryKey: true
+    },
     name: {
         type: Sequelize.STRING,
         defaultValue: 'cereal',
-        validate: 
-        {notEmpty: true,
+        validate:
+        {
+            notEmpty: true,
         }
     },
     cookTime: {
         type: Number,
     },
 });
+
 
 
 module.exports = Recipe;
